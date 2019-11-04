30af90400f27cee79a77d268416ce7314370dd33
ibaquerizo <54192631+ibaquerizo@users.noreply.github.com>
2019-11-03 19:21:59 -0500

    MenuLateral

--- app/src/main/java/com/example/amst6/MainActivity.java
+++ app/src/main/java/com/example/amst6/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.amst6;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity<SplashActivity> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread timer= new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    //Display for 3 seconds
+                    sleep(3000);
+
+
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    //Goes to Activity  StartingPoint.java(STARTINGPOINT)
+                    Intent intent = new Intent(MainActivity.this, MenuLateral.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+
     }
 }
