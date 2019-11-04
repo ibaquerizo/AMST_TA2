bb2e421803ce6ae0097ea65e4cd0fdd4f4b9cd06
ibaquerizo <54192631+ibaquerizo@users.noreply.github.com>
2019-11-03 21:57:59 -0500

    Boton flotante, youtube

--- app/src/main/java/com/example/amst6/MainActivity.java
+++ app/src/main/java/com/example/amst6/MainActivity.java
@@ -2,41 +2,54 @@ package com.example.amst6;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity<SplashActivity> extends AppCompatActivity {
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
+;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Thread timer= new Thread()
-        {
-            public void run()
-            {
-                try
-                {
-                    //Display for 3 seconds
-                    sleep(3000);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
-                }
-                catch (InterruptedException e)
-                {
-                    // TODO: handle exception
-                    e.printStackTrace();
-                }
-                finally
-                {
-                    //Goes to Activity  StartingPoint.java(STARTINGPOINT)
-                    Intent intent = new Intent(MainActivity.this, MenuLateral.class);
-                    startActivity(intent);
-                    finish();
-                }
+      FloatingActionButton fab = findViewById(R.id.fab1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, youtube.class);
             }
-        };
-        timer.start();
+        });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
